22fcbb80489117b68bc3bcf2bb0dabc4cbf2702d
Slain8080 <greycarpendale7@gmail.com>
2025-07-09 14:20:04 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using g3;
+using ThingOne;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        double TableHeight = 200;
+        double TableTopSides = 180;
+        double TableTopDepth = 40;
+        double LegDimensions = 20;
+
+
+        TableGenerator TableGen = new TableGenerator();
+        DMesh3 Table = TableGen.GenerateTable(TableHeight, TableTopSides, TableTopDepth, LegDimensions);
+
+        // string inputPath = @"C:\Users\greyc\Downloads\DragonOriginal.stl";
+        string outputPath = @"C:\Users\greyc\Downloads\Table.obj";
+
+        SaveMesh(Table, outputPath); 
+
+
+    }
+
+    public static void ReduceMeshAndSave(string InputPath, int TriangleCount, string OutputPath)
+    {
+        DMesh3 loadedMesh = null;
+
+        try
+        {
+            loadedMesh = StandardMeshReader.ReadMesh(InputPath);
+            Console.WriteLine($"Mesh read successfully. Vertex count: {loadedMesh.VertexCount}, Triangle count: {loadedMesh.TriangleCount}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Failed to read mesh: {ex.Message}");
+        }
+
+        if (loadedMesh != null) 
+        {
+            Reducer r = new Reducer(loadedMesh);
+            r.ReduceToTriangleCount(TriangleCount);
+
+            Console.WriteLine($"Mesh reduced successfully. New vertex count: {loadedMesh.VertexCount}, New triangle count: {loadedMesh.TriangleCount}");
+
+            IOWriteResult result = StandardMeshWriter.WriteFile(
+                OutputPath,
+                new List<WriteMesh>() { new WriteMesh(loadedMesh) },
+                WriteOptions.Defaults);
+
+            Console.WriteLine($"Reduced Mesh successfully written to new file.");
+        }
+        else
+        {
+            Console.WriteLine("Function Failed, did not export reduced Mesh");
+        }
+
+        
+
+
+    }
+
+    public static void SaveMesh(DMesh3 Mesh, string OutputPath)
+    {
+        IOWriteResult result = StandardMeshWriter.WriteFile(
+                OutputPath,
+                new List<WriteMesh>() { new WriteMesh(Mesh) },
+                WriteOptions.Defaults);
+    }
+
+}
---
+++ TableGenerator.cs
@@ -0,0 +1,80 @@
+﻿using g3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ThingOne
+{
+    public class TableGenerator
+    {
+        // public double TableHeight = 100;
+        // public double TableTopSides = 100;
+        // public double TableTopDepth = 10;
+        // public double LegDimensions = 10;
+
+        public DMesh3 GenerateTable(double TableHeight, double TableTopSides, double TableTopDepth, double LegDimensions)
+        {
+            // Vector3d
+            // Index3i
+            // Box3d, BoxGenerator
+            // 
+
+            Box3d TableTop = new Box3d(Vector3d.Zero, new Vector3d(TableTopSides/2, TableTopSides/2, TableTopDepth/2));
+            Box3d Leg1 = new Box3d(Vector3d.Zero, new Vector3d(LegDimensions/2, LegDimensions/2, TableHeight/2));
+            Box3d Leg2 = new Box3d(Vector3d.Zero, new Vector3d(LegDimensions/2, LegDimensions/2, TableHeight/2));
+            Box3d Leg3 = new Box3d(Vector3d.Zero, new Vector3d(LegDimensions/2, LegDimensions/2, TableHeight/2));
+            Box3d Leg4 = new Box3d(Vector3d.Zero, new Vector3d(LegDimensions/2, LegDimensions/2, TableHeight/2));
+
+
+            // Quaterniond rotation = new Quaterniond(Vector3d.AxisZ, 45); 
+
+            DMesh3 TableTopMesh = BoxToMesh(TableTop);
+            DMesh3 Leg1Mesh = BoxToMesh(Leg1);
+            DMesh3 Leg2Mesh = BoxToMesh(Leg2);
+            DMesh3 Leg3Mesh = BoxToMesh(Leg3);
+            DMesh3 Leg4Mesh = BoxToMesh(Leg4);
+
+
+            // MeshTransforms.Rotate(TableTop, Vector3d.Zero, rotation);
+            MeshTransforms.Translate(TableTopMesh, new Vector3d(0, 0, TableHeight + TableTopDepth / 2));
+
+            double LegCornerXorY = TableTopSides/2 - LegDimensions / 2; 
+
+            MeshTransforms.Translate(Leg1Mesh, new Vector3d(LegCornerXorY, LegCornerXorY, TableHeight / 2));
+            MeshTransforms.Translate(Leg2Mesh, new Vector3d(LegCornerXorY * -1, LegCornerXorY, TableHeight / 2));
+            MeshTransforms.Translate(Leg3Mesh, new Vector3d(LegCornerXorY * -1, LegCornerXorY * -1, TableHeight / 2));
+            MeshTransforms.Translate(Leg4Mesh, new Vector3d(LegCornerXorY, LegCornerXorY * -1, TableHeight / 2));
+
+            DMesh3 Final = new DMesh3();
+            MeshEditor editor = new MeshEditor(Final);
+            editor.AppendMesh(TableTopMesh);
+            editor.AppendMesh(Leg1Mesh);
+            editor.AppendMesh(Leg2Mesh);
+            editor.AppendMesh(Leg3Mesh);
+            editor.AppendMesh(Leg4Mesh);
+
+            return Final;
+
+        }
+
+        public static void SaveMesh(DMesh3 Mesh, string OutputPath)
+        {
+            IOWriteResult result = StandardMeshWriter.WriteFile(
+                    OutputPath,
+                    new List<WriteMesh>() { new WriteMesh(Mesh) },
+                    WriteOptions.Defaults);
+        }
+
+        public static DMesh3 BoxToMesh(Box3d Box)
+        {
+            var generator = new TrivialBox3Generator();
+            generator.Box = Box;
+            generator.Generate();
+            return generator.MakeDMesh();
+        }
+
+    }
+}
